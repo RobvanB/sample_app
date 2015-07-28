3798816c168fd4a2f135a2f9cb54ec15d9c33151
Rob vanBrandenburg <RobvanB@gmail.com>
2015-07-28 16:34:17 -0400

    Make a basic User model (including secure passwords)

--- Gemfile
+++ Gemfile
@@ -3,12 +3,18 @@ source 'https://rubygems.org'
 gem 'rails', '3.2.16'
 gem 'pg', '0.12.2'
 gem 'bootstrap-sass', '2.1'
+gem 'bcrypt-ruby', '3.0.1'
 
 group :development, :test do
-  gem 'sqlite3', '1.3.5'
   gem 'rspec-rails', '2.11.0'
 end
 
+group :development do
+  gem 'annotate', '2.5.0'
+end
+group :test do
+  gem 'capybara', '1.1.2'
+end
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
@@ -19,7 +25,4 @@ end
 
 gem 'jquery-rails', '2.0.2'
 
-group :test do
-  gem 'capybara', '1.1.2'
-end
 
---
+++ app/models/user.rb
@@ -0,0 +1,28 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class User < ActiveRecord::Base
+  attr_accessible :email, :name, :password, :password_confirmation
+  has_secure_password
+
+  before_save { |user| user.email = email.downcase }
+
+  # Validations
+  validates :name, presence: true   # Same as: validates(:name, presence: true) - make sure a non-blank value is provided
+  validates :name, length: { maximum: 50 }
+
+  validates :email, presence: true  # Same as: validates(:email, presence: true)
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, format: { with: VALID_EMAIL_REGEX }
+  validates :email, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
+end
